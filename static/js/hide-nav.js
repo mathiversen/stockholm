afda547f14b2809b45602782eb7dfdb0a708cb5c
Mathias Iversen <work@mathiasiversen.com>
2019-10-27 12:51:31 +0100

    adjusted scroll nav, added button to articles

---
+++ static/js/hide-nav.js
@@ -0,0 +1,32 @@
+(function () {
+    hideShowNav()
+})()
+
+function hideShowNav () {
+    let prevPos = window.pageYOffset;
+    let navOnScroll = document.body.getAttribute("data-scroll-nav")
+    switch (navOnScroll) {
+        case "show":
+            document.body.style.paddingBottom = "50px"
+            window.removeEventListener('scroll', onScroll)
+            break;
+        case "hide":
+            document.body.style.paddingBottom = "0px"
+            window.addEventListener('scroll', onScroll)
+            break;
+        default:
+            console.error("The navigation behaviour can only be [show] or [hide].")
+            break;
+    }
+
+    function onScroll() {
+        let currPos = window.pageYOffset
+        if (prevPos > currPos) {
+        document.getElementById("section-nav").style.transform = "translateY(0)"
+        } else {
+        document.getElementById("section-nav").style.transform = "translateY(100%)"
+        }
+        prevPos = currPos;
+    }
+}
+
